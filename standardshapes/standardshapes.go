72a9b6c842b464022b07a14c8b9e8dd8c1f01bfc
Isaac Chen <49803282+Iscaraca@users.noreply.github.com>
2020-04-29 00:37:47 +0800

    Create standardshapes.go

---
+++ standardshapes/standardshapes.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape struct {
+	area      float64
+	perimeter float64
+}
+
+type Square struct {
+	length float64
+	Shape  // Composition by embedding struct Shape, sort of like inheritance in a way but hierarchy doesn't exist
+}
+
+type equilateralTriangle struct {
+	base  float64
+	Shape // Composition by embedding struct Shape
+}
+
+func (s *Square) setSquDetails() {
+	s.area = s.length * s.length // Set square area as length^2
+	s.perimeter = s.length * 4   // Set square perimeter as length * 4
+}
+
+func (t *equilateralTriangle) setTriDetails() {
+	t.area = t.base * t.base * (math.Sqrt(3) / 4) // Set triangle area as (sqrt(3)/4) * base^2
+	t.perimeter = t.base * 3                      // Set triangle perimeter as base * 3
+}
+
+func main() {
+	GenericShape := Shape{0, 0} // Generic shape struct with area = 0 and perimeter = 0
+
+	Square1 := Square{5, GenericShape} // Object Square1 with length 5
+	Square1.setSquDetails()
+	fmt.Printf("A square with a side length of %.1f has an area of %.1f ", Square1.length, Square1.area)
+	fmt.Printf("and a perimeter of %.1f\n", Square1.perimeter)
+	// A square with a side length of 5.0 has an area of 25.0 and a perimeter of 20.0
+
+	Triangle1 := equilateralTriangle{5, GenericShape} // Object Triangle1 with base 5
+	Triangle1.setTriDetails()
+	fmt.Printf("An equilateral triangle with a side length of %.1f has an area of %.1f ", Triangle1.base, Triangle1.area)
+	fmt.Printf("and a perimeter of %.1f\n", Triangle1.perimeter)
+	// An equilateral triangle with a side length of 5.0 has an area of 10.8 and a perimeter of 15.0
+}

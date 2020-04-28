853ced5fc12e4c6d2b019b96c8e1f477a894b527
Isaac Chen <49803282+Iscaraca@users.noreply.github.com>
2020-04-28 08:50:45 +0800

    Create aplusb.go

---
+++ A+B/aplusb.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(x, y int) (result int) { // Takes in two integers as parameters, returns result
+	result = x + y
+	return
+}
+
+func main() {
+	fmt.Println(add(5, 4))
+	fmt.Println(add(-7, 0))
+}

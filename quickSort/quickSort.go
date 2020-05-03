7b7b1da0b4dc1baa2f85353f34a26ff74c97cefe
Isaac Chen <49803282+Iscaraca@users.noreply.github.com>
2020-05-03 21:47:53 +0800

    Update quickSort.go

--- quickSort/quickSort.go
+++ quickSort/quickSort.go
@@ -7,6 +7,22 @@ import (
 	"strconv"
 )
 
+/*
+	At first, I initialised an array of size 23 (because I read the text
+	file and counted the number of lines) using var arr [23]int. Then I
+	got an error message saying that you cant pass an array of type [23]int
+	into a function that takes a parameter of type []int. What the hell?
+	
+	I then changed the parameters of the functions to take in params of type
+	[23]int instead, but after 5 minutes I realised that that was a stupid way
+	to solve the problem.
+	
+	Finally I realised that i could initialise an array of type []int and append
+	values to it using a function I wrote to extend the length of the array by
+	making a copy of it. I wonder if there's a more memory efficient way to do
+	this. Maybe I could make a copy
+*/
+
 func append(original []int, value int) []int { // Appends values to an array
 	target := make([]int, len(original)+1)
 
@@ -72,6 +88,9 @@ func main() {
 
 	readFile.Close()
 
-	fmt.Println(arr) // Returns [82 28 99 10 54 64 40 50 89 22 5 12 7 17 73 49 96 58 80 81 21 37 98]
-	fmt.Println(quickSort(arr)) // Returns [5 7 10 12 17 21 22 28 37 40 49 50 54 58 64 73 80 81 82 89 96 98 99]
+	fmt.Println(arr)
+	// Returns [82 28 99 10 54 64 40 50 89 22 5 12 7 17 73 49 96 58 80 81 21 37 98]
+	
+	fmt.Println(quickSort(arr))
+	// Returns [5 7 10 12 17 21 22 28 37 40 49 50 54 58 64 73 80 81 82 89 96 98 99]
 }
